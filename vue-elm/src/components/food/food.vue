68bc1e69448d9c93685161b2babcf078502778f3
陈晓勉 <236273522@qq.com>
2017-05-02 17:07:29 +0800

    完善详情页，增加评价筛选

--- vue-elm/src/components/food/food.vue
+++ vue-elm/src/components/food/food.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
     <transition name="move">
-    <div v-show="showFlag" class="food" ref="food">
+    <div v-show="showFlag" class="food" ref="foodWrapper">
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image">
@@ -16,18 +15,107 @@
           <div class="price">
             <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
           </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <transition name="fade">
+          <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
+          </transition>
         </div>
-        <div class="cartcontrol-wrapper">
-          <cartcontrol :food="food"></cartcontrol>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
         </div>
-        <transition name="fade">
-        <div @click="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
-        </transition>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingSelect :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings" @on-result-change="onResultChange" @change-only-content="changeOnlyContent"></ratingSelect>
+          <div class="rating-wrapper">
+            <ul v-show="food.ratings && food.ratings.length">
+              <li v-for="rating in food.ratings" class="rating-item">
+                
+              </li>
+            </ul>
+            <div class="no-rating" v-show="!food.ratings || !food.ratings.length"></div>
+          </div>
+        </div>
       </div>
     </div>
     </transition>
-  </div>
 </template>
+<script>
+import BScroll from 'better-scroll'
+import cartcontrol from '../cartcontrol/cartcontrol'
+import ratingSelect from '../ratingselect/rating-select'
+import split from '../split/split'
+import Vue from 'vue'
+
+const ALL = 2;
+
+export default {
+  props: {
+    food: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      showFlag : false,
+      selectType: ALL,
+      onlyContent: true,
+      desc: {
+        all: '全部',
+        positive: '推荐',
+        negative: '吐槽'
+      }
+    }
+  },
+  created () {
+    this.$nextTick(() => {
+      this._initScroll();
+    });
+  },
+  methods : {
+    _initScroll () {
+      if(!this.foodscroll){
+        this.foodscroll = new BScroll(this.$refs.foodWrapper,{
+          click:true
+        });
+      }else{
+        this.foodscroll.refresh();
+      }
+    },
+    show () {
+      this.showFlag = true;
+      this.selectType = ALL;
+      this.onlyContent = false;
+
+    },
+    hide () {
+      this.showFlag = false;
+    },
+    addFirst (event) {
+      if(!event._constructed){
+        return;
+      }
+      this.$emit('addCart',event.target);
+      Vue.set(this.food,'count',1);
+    },
+    onResultChange(val){
+      this.selectType = val;
+    },
+    changeOnlyContent(){
+      this.onlyContent = !this.onlyContent;
+    }
+  },
+  components : {
+    cartcontrol,
+    ratingSelect,
+    split
+  }
+}
+</script>
 <style lang="stylus" rel="stylesheet/stylus">
   .food
     position:fixed
@@ -39,6 +127,7 @@
     background:#fff
     transition:all 0.3s linear
     transform: translate3d(0,0,0)
+    overflow:auto
     &.move-enter-active,&.move-leave-active
       transform: translate3d(100%,0,0)
     &.move-enter,&.move-leave
@@ -65,6 +154,7 @@
           font-size:20px
     .content
       padding:18px
+      position:relative
       .title
         font-size:14px
         font-weight:700
@@ -94,72 +184,47 @@
           text-decoration:line-through
           font-size:10px
           color:rgb(147,153,159)
-    .cartcontrol-wrapper
-      position:absolute
-      right:12px
-      bottom:12px
-    .buy
-      position:absolute
-      right:18px
-      bottom:18px
-      z-index:10
-      height:24px
-      line-height:24px
-      padding:0 12px
-      box-sizing:border-box
-      border-radius:12px
-      font-size:10px
-      color:#fff
-      background:rgb(0,160,220)
-      transition: all 0.4s linear
-      opacity:1
-      &.fade-enter-active,&.fade-leave-active
-        opacity:0
-      &.fade-enter,&.fade-leave
-        opacity:0
+      .cartcontrol-wrapper
+        position:absolute
+        right:12px
+        bottom:12px
+      .buy
+        position:absolute
+        right:18px
+        bottom:18px
+        z-index:10
+        height:24px
+        line-height:24px
+        padding:0 12px
+        box-sizing:border-box
+        border-radius:12px
+        font-size:10px
+        color:#fff
+        background:rgb(0,160,220)
+        transition: all 0.4s linear
+        opacity:1
+        &.fade-enter-active,&.fade-leave-active
+          opacity:0
+        &.fade-enter,&.fade-leave
+          opacity:0
+    .info
+      padding:18px
+      .title
+        line-height:14px
+        margin-bottom:6px
+        font-size:14px
+        color:rgb(7,17,27)
+      .text
+        font-size:12px
+        font-weight:200
+        line-height:24px
+        padding:0 8px
+        color:rgb(77,85,93)
+    .rating
+      padding-top:18px
+      .title
+        line-height:14px
+        margin-left:18px
+        font-size:14px
+        color:rgb(7,17,27)
 </style>
-<script>
-import BScroll from 'better-scroll'
-import cartcontrol from '../cartcontrol/cartcontrol'
-import Vue from 'vue'
-
-export default {
-  props: {
-    food: {
-      type: Object
-    }
-  },
-  data () {
-    return {
-      showFlag : false
-    }
-  },
-  methods : {
-    show () {
-      this.showFlag = true;
-      this.$nextTick(() => {
-        if(!this.scroll){
-          this.scroll = new BScroll(this.$refs.food,{
-            click:true
-          });
-        }else{
-          this.scroll.refresh();
-        }
-      });
-    },
-    hide () {
-      this.showFlag = false;
-    },
-    addFirst (event) {
-      if(!event._constructed){
-        return;
-      }
-      this.$emit('addCart',event.target);
-      Vue.set(this.food,'count',1);
-    }
-  },
-  components : {
-    cartcontrol
-  }
-}
-</script>
