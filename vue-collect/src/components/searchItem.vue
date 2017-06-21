c161f28895cd9df8fef976bc12d431952cea2cf6
chenxiaomian <chenxiaomian@kmb2b.com>
2017-06-21 18:05:45 +0800

    提交搜索逻辑

--- vue-collect/src/components/searchItem.vue
+++ vue-collect/src/components/searchItem.vue
@@ -6,21 +6,24 @@
         <form class="weui-search-bar__form" onsubmit="return false;">
             <div class="weui-search-bar__box">
                 <i class="weui-icon-search"></i>
-                <input type="input" class="weui-search-bar__input" placeholder="请输入关键词搜索" autocomplete="off" v-model="searchKey" lazy/>
-                <a href="javascript:" class="weui-icon-clear" @click="cancelSearch" v-show="isActive"></a>
+                <input type="input" class="weui-search-bar__input" placeholder="请输入关键词搜索" @keyup="search()" v-model="keyword"/>
+                <a href="javascript:" class="weui-icon-clear" @click="clean" v-show="isActive"></a>
             </div>
         </form>
-        <a href="javascript:" class="weui-search-bar__cancel-btn" @click="cancelSearch" v-show="isActive">取消</a>
+        <a href="javascript:" class="weui-search-bar__cancel-btn" @click="clean" v-show="isActive">取消</a>
         </div>
     </header>
     <div class="weui-cells searchbar-result">
+    	<!-- <li v-for="item in result">
+		    {{ item.market_name }}
+		</li> -->
         <a class="weui-cell weui-cell_access searchbar-item" href="javascript:;" v-for="item in result">
-        	<div class="weui-cell__bd weui-cell_primary"><p>{{item.market_name}}{{item.market_id}}</p></div><div class="weui-cell__ft"></div>
+        	<div class="weui-cell__bd weui-cell_primary" v-bind:id="item.market_id"><p>{{item.market_name}}</p></div><div class="weui-cell__ft"></div>
         </a>
     </div>
     <div class="emptyContent" v-show="nothing">
         <i class="ic_none_reminder"></i>
-        <p class="emptyTip">对不起，没有与“{{searchKey}}”相关的内容</p>
+        <p class="emptyTip">对不起，没有与“{{keyword}}”相关的内容</p>
     </div>
 </div>
 </template>
@@ -31,74 +34,88 @@ import store from 'store';
 export default {
 	data () {
 		return {
-			searchKey:'',
+			keyword:'',
 			isActive: false,
 			nothing: false,
-			result:{},
-			query: this.$route.query
+			result:[{
+				market_id:7,
+				market_name:'广州市清平中药材专业市场'
+			}],
+			query: this.$route.query,
+			url:{
+				market: '/likeMarket',
+				medicine: '/likeMedicine',
+				cmedicine: '/likeMedicine',
+				base: '/likeBase',
+				grower: '/likeGrower',
+				manufacturer: '/likeManufacturer'
+			}
 		}
 	},
 	created () {
-		console.log(this.query.key);
+		// console.log(this.query.key);
 	},
 	methods: {
 		backTo: function(){
 			this.$router.go(-1);
 		},
-		tranUrl: function(val){
-			var url = '';
-	        switch(val){
-	            case 'market':
-	                url = '/likeMarket';
-	                break;
-	            case 'medicine':
-	                url = '/likeMedicine';
-	                break;
-	            case 'cmedicine':
-	                url = '/likeMedicine';
-	                break;
-	            case 'base':
-	                url = '/likeBase';
-	                break;
-	            case 'grower':
-	                url = '/likeGrower';
-	                break;
-	            case 'manufacturer':
-	                url = '/likeManufacturer';
-	                break;
-	            default:
-	                ;
-	        }
-	        return url;
-		},
-		cancelSearch: function(){
+		clean: function(){
 			this.isActive = false;
-			this.searchKey = '';
+			this.nothing = false;
+			this.keyword = '';
+			this.result = [];
 		},
-		SearchInterface:function(val){
-			var _this = this,
+		search:function(ev){
+			var timer,
+				_this = this,
 				jsonData = {},
-				key = this.tranUrl(this.query.key);
-	        jsonData.userName = store.get('loginName');
-	        jsonData.password = store.get('password');
-	        jsonData[this.query.key] = val;
+				key = _this.url[_this.query.key];
+
+			jsonData.userName = 'hch';
+	        jsonData.password = 'e10adc3949ba59abbe56e057f20f883e';
+	        // jsonData.userName = store.get('loginName');
+	        // jsonData.password = store.get('password');
+	        jsonData[_this.query.key] = _this.keyword;
+
+			if(timer || jsonData[_this.query.key]==''){
+				_this.nothing = false;
+				_this.result = [];
+				return;
+			};
+
+			timer = setTimeout(function(){
+				_this.result = [];
+				_this.post(key,jsonData);
+				clearTimeout(timer);
+                timer = null;
+			},500);
+		},
+		post:function(key,jsonData){
+			var _this = this;
+
 			this.$http.jsonp(this.$store.state.kmUrl+key,{
               params : {"parms":JSON.stringify(jsonData)},
               jsonp : 'jsoncallback'
             }).then(function(res){
-              _this.result = res.body;
+              // console.log(JSON.parse(res.body.message));
+              var arr = JSON.parse(res.body.message);
+              if(arr&&arr!=''){
+              	_this.result = JSON.parse(res.body.message);
+              	_this.nothing = false;
+              }else{
+              	_this.nothing = true;
+              }
             },function(err){
-              console.log(err);
+              // console.log(err);
             });
 		}
 	},
 	watch: {
-		searchKey:function(val){
+		keyword:function(val){
 			if(val===''){
 				this.isActive = false;
 			}else{
-				this.isActive = true;
-				// this.SearchInterface(val);
+				this.isActive = true;				
 			}
 		}
 	}
